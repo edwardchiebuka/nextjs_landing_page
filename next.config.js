f41a9c92b845865d64101eeee933f8ac2b7f82d2
edwardchiebuka <edwardchiebuka@gmail.com>
2022-03-27 07:06:00 +0100

    loader

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+        loader: "akamai",
+        path: "",
+    }
+}
+
 const withPlugins = require('next-compose-plugins');
 const optimizedImages = require('next-optimized-images');
 
-module.exports = withPlugins([optimizedImages], { target: 'serverless' });
+
+
+module.exports = withPlugins([optimizedImages], { target: 'serverless' }, nextConfig);
